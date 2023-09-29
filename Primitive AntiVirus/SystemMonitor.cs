ac29243294a53497bf74b33972bb0c5e64920a16
Moomove <louisreaysmith@gmail.com>
2023-09-29 10:00:40 +1000

    init
    initial functions and param createed

---
+++ Primitive AntiVirus/SystemMonitor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Primitive_AntiVirus
+{
+    class SystemMonitor
+    {
+        //public PList listofCheckedProcess
+        public int listOfCheckedProcess;
+        //public Process uncheckedProcess
+        public int uncheckedProcess;
+
+
+
+        public void SystemBoot()
+        {
+
+        }
+        public void CheckProcess()
+        {
+
+        }
+        public void AllowedProcess()
+        {
+
+        }
+        public void PassToCompBD()
+        {
+
+        }
+        public void AnalyiseProcess()
+        {
+
+        }
+        public void KillProcess()
+        {
+
+        }
+        public void PromptUser()
+        {
+
+        }
+        public void UpdateList()
+        {
+
+        }
+    }
+}
