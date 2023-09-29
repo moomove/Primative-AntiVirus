fb9940ec91605c4eaefdb90ab500fca4e010f137
Moomove <louisreaysmith@gmail.com>
2023-09-29 13:18:53 +1000

    update

--- Primitive AntiVirus/SystemMonitor.cs
+++ Primitive AntiVirus/SystemMonitor.cs
@@ -1,46 +1,92 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Primitive_AntiVirus
 {
+    public class Process
+    {
+        int pID;
+        string pName;
+    }
+    public class listOfCheckedProcess
+    {
+        public List<Process> PList;
+    }
+
     class SystemMonitor
     {
-        //public PList listofCheckedProcess
-        public int listOfCheckedProcess;
-        //public Process uncheckedProcess
-        public int uncheckedProcess;
+        
+        static void Main()
+        {
+            SystemBoot();
+        }
 
-
-
-        public void SystemBoot()
+        public static void SystemBoot()//gets called on load and sets up the rest of the system
+        {
+            var uncheckedList = GeneratePList();
+            foreach (Process process in uncheckedList)
+            {
+                CheckProcess(uncheckedList.process);
+            }
+        }
+        public static void CheckProcess(Process p)//quick check of a process
+        {
+            AnalyiseProcess(p);
+        }
+        public static void AllowedProcess()//maybe this should just be a call to the comparisonDB
         {
 
         }
-        public void CheckProcess()
+        public static void PassToCompBD()
         {
 
         }
-        public void AllowedProcess()
+        public static void AnalyiseProcess(Process p)//indepth check of a process
         {
 
         }
-        public void PassToCompBD()
+        public static void KillProcess(Process p)
         {
-
+            Console.WriteLine($"Killing {p}");
+            p.Kill();
+            Sleep(3);//sleep to see if the process reboots itself
+            CheckProcess(p);
+            if (p == null)
+            {
+                Console.WriteLine("Successfully killed process");
+            }
+            else
+            {
+                Console.WriteLine("Unable to kill Process");
+            }
         }
-        public void AnalyiseProcess()
+        public static void PromptUser(Process p)
         {
-
+            Console.WriteLine($"{p} Is suspected of being suspicious ");
+            UpdateList(p);
         }
-        public void KillProcess()
+        public static void UpdateList(Process p)
         {
+            
+            Console.WriteLine("Add Process to WhiteList [W] or kill process and add to BalckList [B] or any other key to Cancel");
+            string c = Console.ReadLine();
 
-        }
-        public void PromptUser()
-        {
+            if(c == "w" ||c == "W")
+            {
+                Console.WriteLine("Adding to WhiteList");
 
-        }
-        public void UpdateList()
-        {
+            }
+            else if (c == "b" || c == "B")
+            {
+                Console.WriteLine("Adding to BlackList");
+
+            }
+            else
+            {
+                Console.WriteLine("Cancelled");
+            } 
 
         }
     }
