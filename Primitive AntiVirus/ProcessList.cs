8d91babe8a177833acebd728529a0fb2ca7ecb4c
moomove <louisreaysmith@gmail.com>
2023-10-15 19:55:38 +1100

    slightlyfixed

--- Primitive AntiVirus/ProcessList.cs
+++ Primitive AntiVirus/ProcessList.cs
@@ -4,80 +4,83 @@ using System.Collections.Generic;
 
 namespace Primitive_AntiVirus
 {
+    public class SysProcess
+    {
+        int ProcessID;
+        string ProcessName;
+        bool PotentallyMalicous;
+        bool WhiteListed;
+        bool BlackListed;
+    }
+
     class ProcessList
     {
-        public class SysProcess
+
+        public List<SysProcess> pList;
+
+
+        var whiteList = new List<string>();
+        var blackList = new List<string>();
+        var greyList = new List<string>();
+            
+
+        public void AddToWhiteList(string processName)
         {
-            int ProcessID;
-            string ProcessName;
-            bool PotentallyMalicous;
-            bool WhiteListed;
-            bool BlackListed;
-        }
-            public List<SysProcess> pList;
-
-            {
-                whiteList = new List<string>();
-                blackList = new List<string>();
-                greyList = new List<string>();
-            }
-
-            public void AddToWhiteList(string processName)
-            {
             whiteList.Add(processName);
+        }
+
+        public void AddToBlackList(string processName)
+        {
+            blackList.Add(processName);
+        }
+
+        public void AddToGreyList(string processName)
+        {
+            greyList.Add(processName);
+        }
+
+        public bool IsProcessAllowed(string processName)
+        {
+            if (whiteList.Contains(processName))
+            {
+                return true;
+            }
+            else if (blackList.Contains(processName))
+            {
+                return false;
+            }
+            else
+            {
+                // If the process is not in the whitelist or blacklist, it is in the greylist.
+                // We can either allow the process or block it based on our policies.
+                // For example, we can allow the process if it is signed by a trusted publisher.
+                return true;
+            }
+        }
+
+        public List<Process> GetRunningProcesses()
+        {
+            List<Process> processes = new List<Process>();
+
+            foreach (Process process in Process.GetProcesses())
+            {
+                processes.Add(process);
             }
 
-    public void AddToBlackList(string processName)
-    {
-        blackList.Add(processName);
-    }
-
-    public void AddToGreyList(string processName)
-    {
-        greyList.Add(processName);
-    }
-
-    public bool IsProcessAllowed(string processName)
-    {
-        if (whiteList.Contains(processName))
-        {
-            return true;
-        }
-        else if (blackList.Contains(processName))
-        {
-            return false;
-        }
-        else
-        {
-            // If the process is not in the whitelist or blacklist, it is in the greylist.
-            // We can either allow the process or block it based on our policies.
-            // For example, we can allow the process if it is signed by a trusted publisher.
-            return true;
-        }
-    }
-
-    public List<Process> GetRunningProcesses()
-    {
-        List<Process> processes = new List<Process>();
-
-        foreach (Process process in Process.GetProcesses())
-        {
-            processes.Add(process);
+            return processes;
         }
 
-        return processes;
-    }
-
-    public void ScanProcesses()
-    {
-        List<Process> processes = GetRunningProcesses();
-
-        foreach (Process process in processes)
+        public void ScanProcesses()
         {
-            if (!IsProcessAllowed(process.ProcessName))
+            List<Process> processes = GetRunningProcesses();
+
+            foreach (Process process in processes)
             {
-                // The process is not allowed, so we can block it.
-                process.Kill();
+                if (!IsProcessAllowed(process.ProcessName))
+                {
+                    // The process is not allowed, so we can block it.
+                    process.Kill();
+                }
             }
         }
     }
