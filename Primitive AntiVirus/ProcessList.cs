ad93b4c2e1ff8af0f5bf9b1a2e0c85e79daabd08
mtowhidutas <rahitonmoy@gmail.com>
2023-10-15 19:21:36 +0600

    Plist Code

--- Primitive AntiVirus/ProcessList.cs
+++ Primitive AntiVirus/ProcessList.cs
@@ -14,11 +14,74 @@ namespace Primitive_AntiVirus
             bool WhiteListed;
             bool BlackListed;
         }
-        public List<SysProcess> pList;
+            public List<SysProcess> pList;
 
-        public static void GeneratePList()
+            {
+                whiteList = new List<string>();
+                blackList = new List<string>();
+                greyList = new List<string>();
+            }
+
+            public void AddToWhiteList(string processName)
+            {
+            whiteList.Add(processName);
+            }
+
+    public void AddToBlackList(string processName)
+    {
+        blackList.Add(processName);
+    }
+
+    public void AddToGreyList(string processName)
+    {
+        greyList.Add(processName);
+    }
+
+    public bool IsProcessAllowed(string processName)
+    {
+        if (whiteList.Contains(processName))
         {
+            return true;
+        }
+        else if (blackList.Contains(processName))
+        {
+            return false;
+        }
+        else
+        {
+            // If the process is not in the whitelist or blacklist, it is in the greylist.
+            // We can either allow the process or block it based on our policies.
+            // For example, we can allow the process if it is signed by a trusted publisher.
+            return true;
+        }
+    }
 
+    public List<Process> GetRunningProcesses()
+    {
+        List<Process> processes = new List<Process>();
+
+        foreach (Process process in Process.GetProcesses())
+        {
+            processes.Add(process);
+        }
+
+        return processes;
+    }
+
+    public void ScanProcesses()
+    {
+        List<Process> processes = GetRunningProcesses();
+
+        foreach (Process process in processes)
+        {
+            if (!IsProcessAllowed(process.ProcessName))
+            {
+                // The process is not allowed, so we can block it.
+                process.Kill();
+            }
         }
     }
 }
+
+
+
