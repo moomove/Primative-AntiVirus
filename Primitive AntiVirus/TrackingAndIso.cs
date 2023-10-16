9c52ddd654dd45dedfb665ba54c0d221e0a6b741
moomove <louisreaysmith@gmail.com>
2023-10-16 19:32:44 +1100

    functionOutline

--- Primitive AntiVirus/TrackingAndIso.cs
+++ Primitive AntiVirus/TrackingAndIso.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace Primitive_AntiVirus
 {
-
     class TrackingAndIso
     {
+        public SysProcess maliciousProcess;
+        public bool success;
+        public bool processSource;
+
+        public static void TrackProcess(SysProcess maliciousProcess)
+        {
+
+        }
+
+        public static void AlertUser()//Alert the user
+        {
+
+        }
     }
 }
