3af9ff941e1e2b88a4ec7ef25b824699e772ee9f
Hiru Ihalakorala <ihalai@utas.edu.au>
2023-10-18 21:12:39 +1100

    f

--- Primitive AntiVirus/Comparison.cs
+++ Primitive AntiVirus/Comparison.cs
@@ -51,7 +51,7 @@ namespace Primitive_AntiVirus
                     float cpuUsage = GetCpuUsage(process);
 
                     // Check if either memory or CPU usage is above 25%
-                    if (memoryUsage > 1.0f || cpuUsage > 1.0f)
+                    if (memoryUsage > 25.0f || cpuUsage > 25.0f)
                     {
                         // Prompt the user to add the process to the BlackList
                         Console.WriteLine($"Process {process.ProcessName} has high resource usage. Do you want to add it to the BlackList? (yes/no)");
