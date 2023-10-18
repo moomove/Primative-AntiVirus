b7299c20760bc16bd045bc8b56173e3bb078964a
Moomove <louisreaysmith@gmail.com>
2023-10-18 16:48:15 +1100

    menu

--- Primitive AntiVirus/SystemMonitor.cs
+++ Primitive AntiVirus/SystemMonitor.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Primitive_AntiVirus
 {
@@ -16,10 +17,11 @@ namespace Primitive_AntiVirus
     class SystemMonitor
     {
         //public List<ProcessList.SysProcess> CheckedProccess;
-
+       
         static void Main()
         {            
             SystemBoot();
+            MaintainSystem();
             Console.ReadLine();
            
         }
@@ -31,9 +33,81 @@ namespace Primitive_AntiVirus
             {
                 Console.WriteLine(process.WorkingSet64 +" "+ process.ProcessName );
                 AnalyiseProcess(process);
+                
             }
         }
-        public static void 
+        public static async void MaintainSystem() {
+            bool run = true;
+            while (run)
+            {
+                CancellationTokenSource tokenSource = new CancellationTokenSource();
+                CancellationToken token = tokenSource.Token;
+
+                Console.WriteLine("...System being monitored...");
+                Console.WriteLine("");
+                Console.WriteLine("Select an operations you would like to run");
+                Console.WriteLine("");
+                Console.WriteLine("\t1. Rescan System");
+                Console.WriteLine("\t2. View BlackListed Processes");
+                Console.WriteLine("\t3. View WhiteListed Processes");
+                Console.WriteLine("\t4. Kill Process");
+                Console.WriteLine("\t5. Shutdown Self");
+                Console.WriteLine("\t6. Trace Process");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Task userInputTask = WaitForUserInputAsync(token);
+
+                int waitMinutes = 1;//how many minutes to wait
+                int refreshPeriod = waitMinutes * 60000; //the amount of microseconds in a minute
+                Task timeoutTask = Task.Delay(refreshPeriod);
+
+                // Wait for either user input or a timeout
+                Task completedTask = await Task.WhenAny(userInputTask, timeoutTask);
+
+                if (completedTask == userInputTask)
+                {
+                    // User provided input
+                    Console.WriteLine("...System Monitoring Paused...");
+                    string option = Console.ReadLine();
+
+                    if(option == "1")
+                    {
+                        Console.WriteLine("1234123");
+                    }
+                    else
+                    {
+                        Console.WriteLine("1234");
+                    }
+                }
+                else
+                {
+                    // Timeout occurred
+                    //rescan system
+                    Console.WriteLine("rescanning");
+                }
+
+                // Optionally cancel the user input task if it's still running
+                if (!userInputTask.IsCompleted)
+                {
+                    tokenSource.Cancel();
+                }
+            }
+        }
+        static async Task WaitForUserInputAsync(CancellationToken token)
+        {
+            var inputTask = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        Console.ReadKey(intercept: true); // Read and discard the key press
+                        break;
+                    }
+                }
+            });
+            await inputTask;
+        }
 
         public static void CheckProcess(Process p)//quick check of a process
         {
